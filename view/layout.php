ba76433a74a6dc2ed96c5f09113e4554b9d30492
Squirrel-red <elena.spb@sfr.fr>
2024-05-21 12:16:59 +0200

    tests coté Admin --> Utilisateurs/Liste des utilisateurs

--- view/layout.php
+++ view/layout.php
@@ -48,8 +48,12 @@
                                                 <?php
                                                 // si l'utilisateur est connecté 
                                                 ?>
+                                                
                                                 <li class="link"><a href="index.php?ctrl=security&action=userProfile&id=<?= App\Session::getUser()->getId() ?>">Profil</a>
                                                 </li>
+
+                                                <!-- pour tester l'accès à la liste des utilisateurs car Session isAdmin ne marche pas -->
+                                                <!-- <li class="link"><a href="index.php?ctrl=security&action=users">Utilisateurs</a></li> -->
 
                                                 <li class="link"><a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                                                 </li>
